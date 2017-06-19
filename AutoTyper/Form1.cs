c6ccf1aa4ce1ec52501207198cff9cd1e5b28e7c
moselhy <mohamed.moselhy@uwo.ca>
2017-06-19 19:39:18 -0400

    Initial commit

---
+++ AutoTyper/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace AutoTyper
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            var gkh = new globalKeyboardHook();
+            gkh.HookedKeys.Add(Keys.F5);
+            gkh.KeyDown += gkh_KeyDown;
+
+            timer1.Interval = 1000;
+        }
+
+        void gkh_KeyDown(object sender, KeyEventArgs e)
+        {
+            button1.PerformClick();
+            e.Handled = true;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = !timer1.Enabled;
+            button1.Text = timer1.Enabled ? "Stop" : "Start";
+            button1.Text += @" (F5)";
+            Text = timer1.Enabled ? "Started" : "Stopped";
+            Text += @" Autotyper";
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            SendKeys.Send(textBox1.Text.Substring(0,1));
+        }
+    }
+}
